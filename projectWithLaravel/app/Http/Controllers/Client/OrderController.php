0d9160add8c691c5e616244f0fe62d4346c8ddcc
Uyen <20522138@gm.uit.edu.vn>
2022-12-08 15:47:01 +0700

    Order+ProductPicture

--- projectWithLaravel/app/Http/Controllers/Client/OrderController.php
+++ projectWithLaravel/app/Http/Controllers/Client/OrderController.php
@@ -14,9 +14,13 @@ class OrderController extends Controller
         $donhangs = DB::table('donhang')->get()->where('KhachHangID', Auth::guard('khachhang')->user()->id);
         $chitietdonhangs = array();
         foreach ($donhangs as $donhang) {
-            $chitietdonhang = DB::table('chitiet_donhang')->get()->where('DonHangID', $donhang->DonHangID);
+            $chitietdonhang = DB::table('chitiet_donhang')
+                ->join('sanpham', 'chitiet_donhang.SanPhamID', '=', 'sanpham.SanPhamID')
+                ->select('chitiet_donhang.*', 'sanpham.SanPhamTen', 'sanpham.Gia', 'sanpham.HinhAnh')->get()
+                ->where('DonHangID', $donhang->DonHangID);
             array_push($chitietdonhangs, $chitietdonhang);
         }
+        // dd($donhangs, $chitietdonhangs);
         return view('client.my-account.my-order', ['donhangs' => $donhangs, 'chitietdonhangs' => $chitietdonhangs]);
     }
 }
