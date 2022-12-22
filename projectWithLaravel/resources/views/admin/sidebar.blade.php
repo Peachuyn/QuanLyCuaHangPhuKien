5d67b9b3b0faba718851d51d0170de7577da5ef1
PhgDuyn <20520169@gm.uit.edu.vn>
2022-12-22 10:57:07 +0700

    Update-AdminProduct

--- projectWithLaravel/resources/views/admin/sidebar.blade.php
+++ projectWithLaravel/resources/views/admin/sidebar.blade.php
@@ -64,6 +64,33 @@
                   </li>
                 </ul>
               </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="nav-icon fas fa-tachometer-alt"></i>
+                  <p>
+                    Quản lí sản phẩm
+                    <i class="right fas fa-angle-left"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+
+                  <li class="nav-item">
+                    <a href="/admin/products/add" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Thêm sản phẩm</p>
+                    </a>
+                  </li>
+                  
+                  <li class="nav-item">
+                    <a href="/admin/menus/list" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Danh sách sản phẩm</p>
+                    </a>
+                  </li>
+                  
+                </ul>
+              </li>
+
 
             </ul>
           </nav>
