a47d5c605f115892193171cc66428aecba741d7f
Huỳnh Yến Anh <hyenanh017@gmail.com>
2022-12-24 23:15:27 +0700

    sidebar fix

--- projectWithLaravel/resources/views/admin/sidebar.blade.php
+++ projectWithLaravel/resources/views/admin/sidebar.blade.php
@@ -1,8 +1,8 @@
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <aside class="main-sidebar sidebar-dark-primary elevation-4 side-bar-menu" style='background-color: #7572a6 ; font: #fcf8fc' >
       <!-- Brand Logo -->
       <a href="/admin" class="brand-link">
-        <img src="/template/admin/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="/template/admin/dist/img/AdminLTELogo.png" alt="Admin Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Bellezza</span>
       </a>
 
@@ -11,10 +11,10 @@
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
-            <img src="/template/admin/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            <img src="/template/admin/dist/img/actress.png" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
-            <a href="#" class="d-block">Alexander Pierce</a>
+            <a href="#" class="d-block">admin</a>
           </div>
         </div>
         <!-- SidebarSearch Form -->
